627ed25fad0205e73534adce6889881cf763d247
Vivek Raj <81849302+vivek625@users.noreply.github.com>
2022-09-29 15:40:28 +0530

    House_pattern.py

---
+++ House_pattern.py
@@ -0,0 +1,11 @@
+for i in range(1,10):
+    for j in range(1,10):
+        if(i==4 or j==9 or i==9 or i==4  or i+j==4 or j == 4 or i*j == 7 or i *j == 8 or i*j == 6 or i*j == 5):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print() 
+  
+  
+  #VivekRajSingh
+  
